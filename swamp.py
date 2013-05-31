d0793392ed3193f50d1cdad3d608bfd33afe6199
AlexBroccoli <alex@qwoot.net>
2013-05-31 19:33:31 +0200

    added urls contexts and event join loop

--- swamp.py
+++ swamp.py
@@ -17,42 +17,58 @@ from bs4 import BeautifulSoup
 from urlparse import urljoin
 
 def main():
-	base_url = 'http://azarius.local'
-	pool = Pool(2)
-	spawner = Spawner(pool, base_url)
-	spawner.add(base_url)
-	spawner.wait()
+	url = 'http://azarius.shulgin'
+	pool = Pool(5)
+	spawner = Spawner(pool)
+
+	# context in which new urls are relative to starting url
+	context = URLContext(url, url)
+
+	spawner.add(context)
+
+	try:
+		spawner.wait()
+	except KeyboardInterrupt:
+		print ' Abort.'
+		return
 
 class Spawner():
 
 	pool = None
 
+	found = None
+
 	jobs = []
 
 	base_url = None
 
-	def __init__(self, pool, base_url):
+	def __init__(self, pool):
+		self.found = set()
 		self.pool = pool
-		self.base_url = base_url
 
 	def add(self, item):
 		def job(self, item):
-			print 'start'
-			handle = Handle(item, self.base_url)
-			found = handle.process()
-			for new in found:
-				self.add(new)
+			self.process_result(Handle(item).process())
 
 		self.jobs.append(self.pool.spawn(job, self, item))
 
+	def process_result(self, found):
+		# remove urls we have found already
+		found = set(found) - self.found
+
+		for new in found:
+			self.add(new)
+
+		# update already found urls with the newly found ones
+		self.found |= set(found)
+
 	def wait(self):
-		gevent.joinall(self.jobs)
+		while len(self.jobs) > 0:
+			gevent.joinall(self.jobs)
 
 class Handle():
 
-	base_url = None
-
-	url = None
+	context = None
 
 	method = 'get'
 
@@ -60,41 +76,69 @@ class Handle():
 
 	_hash = None
 
-	def __init__(self, url, base_url):
-		self.url = url
-		self.base_url = base_url
+	def __init__(self, context):
+		self.context = context
 
 	def __hash__(self):
 		if self._hash == None:
 			#FIXME shorten self.url a bit
-			s = ':'.join([self.method, self.url] + self.fields)
+			s = ':'.join([self.method, self.context] + self.fields)
 			m = md5()
 			m.update(s)
 			self._hash = m.hexdigest().__hash__()
 
 		return self._hash
 
-
 	def process(self):
-		url = urljoin(self.base_url, self.url)
-		print url
-		r = requests.get(url)
+		url = urljoin(self.context.referer, self.context.url)
+
+		try:
+			r = requests.get(url)
+		except Exception as e:
+			print 'Error during request:', str(e)
+			return []
+
+		print r.status_code, url
 
 		if (r.status_code != 200):
-			return
+			return []
 
-		return self.content(r.text)
+		try:
+			text = r.text
+		except TypeError:
+			# this does not seem to be test, skip
+			return []
+
+		return self.content(text)
 
 	def content(self, body):
-		soup = BeautifulSoup(body)
-		atags = soup.find_all('a')
 		found = []
 
+		try:
+			soup = BeautifulSoup(body)
+		except Exception:
+			print 'Document could not be parsed!'
+			return found
+
+		atags = soup.find_all('a')
+
 		for atag in atags:
-			found.append(atag.get('href'))
+			new = URLContext(atag.get('href'), self.context.url)
+			found.append(new)
 
 		return found
 
+class URLContext():
+
+	url = None
+
+	referer = None
+
+	method = 'get'
+
+	def __init__(self, url, referer):
+		self.url = url
+		self.referer = referer
 
 if __name__ == '__main__':
 	main()
